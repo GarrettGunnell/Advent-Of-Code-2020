e9c82f65acd0e85a7f0cfb723845d88f84d4fd65
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-12-09 02:43:44 -0800

    Add day 9 bogosolution

---
+++ 9/bogosolution.py
@@ -0,0 +1,15 @@
+import random
+
+inputFile = open('input.txt', 'r')
+inputLines = inputFile.readlines()
+
+input = []
+for line in inputLines:
+    line = line.strip()
+    input.append(int(line))
+
+while True:
+    start = random.randint(0, len(input) - 1)
+    end = random.randint(start, len(input) - 1)
+    if sum(input[start:end]) == 1398413738:
+        exit(print(min(input[start:end]) + max(input[start:end])))
