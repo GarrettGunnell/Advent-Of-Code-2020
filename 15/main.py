8e6a2604d3a79bd2cb45e77bec918eafea0bfb52
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-12-14 21:29:11 -0800

    Add day 15 solutions

---
+++ 15/main.py
@@ -0,0 +1,57 @@
+inputFile = open('input.txt', 'r')
+inputLines = inputFile.readlines()
+
+input = []
+for line in inputLines:
+    line = line.strip().split(',')
+    for i in line:
+        input.append(int(i))
+
+def silver(input):
+    spoken = {}
+    turn = 0
+    last_spoken = 0
+    last_difference = 0
+
+    for num in input:
+        turn += 1
+        spoken[num] = turn
+        last_spoken = num
+
+    while turn != 2020:
+        turn += 1
+        last_spoken = last_difference
+        if last_spoken in spoken:
+            last_difference = turn - spoken[last_spoken]
+        else:
+            last_difference = 0
+
+        spoken[last_spoken] = turn
+
+    return last_spoken
+
+def gold(input):
+    spoken = {}
+    turn = 0
+    last_spoken = 0
+    last_difference = 0
+
+    for num in input:
+        turn += 1
+        spoken[num] = turn
+        last_spoken = num
+
+    while turn != 30000000:
+        turn += 1
+        last_spoken = last_difference
+        if last_spoken in spoken:
+            last_difference = turn - spoken[last_spoken]
+        else:
+            last_difference = 0
+
+        spoken[last_spoken] = turn
+
+    return last_spoken
+
+print(silver(input))
+print(gold(input))
